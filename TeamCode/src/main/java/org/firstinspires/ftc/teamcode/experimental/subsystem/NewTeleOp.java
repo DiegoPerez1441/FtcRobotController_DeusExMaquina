97ede011a7ec6f8b46afa76ffb627561aef799f2
DiegoPerez1441 <devdiegoperez@gmail.com>
2021-01-25 17:55:29 -0600

    Moved 'subsystem' package into 'experimental'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experimental/subsystem/NewTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experimental/subsystem/NewTeleOp.java
@@ -27,14 +27,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.subsystem;
+package org.firstinspires.ftc.teamcode.experimental.subsystem;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.subsystem.MecanumDrivetrain;
 
 
 /**
