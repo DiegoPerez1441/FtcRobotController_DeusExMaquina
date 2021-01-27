83eaf59f5f6226f325bed861ad650d0c161ba5f6
DiegoPerez1441 <devdiegoperez@gmail.com>
2021-01-26 23:55:30 -0600

    Set targetZone corresponding to initial ring stack size

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorFlowObjectDetection.java
@@ -38,6 +38,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.enums.WobbleTargetZone;
 
 import java.util.List;
 
@@ -57,6 +58,8 @@ public class TensorFlowObjectDetection extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad";
     private static final String LABEL_SECOND_ELEMENT = "Single";
+    private String stackSize = "None"; // Default
+    private WobbleTargetZone targetZone = WobbleTargetZone.BLUE_A; // Default
 
     /*
      * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
@@ -122,17 +125,27 @@ public class TensorFlowObjectDetection extends LinearOpMode {
                     // the last time that call was made.
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
                     if (updatedRecognitions != null) {
-                      telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
 
-                      // step through the list of recognitions and display boundary info.
-                      int i = 0;
-                      for (Recognition recognition : updatedRecognitions) {
-                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                          recognition.getLeft(), recognition.getTop());
-                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                recognition.getRight(), recognition.getBottom());
-                      }
+                        // step through the list of recognitions and display boundary info.
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                    recognition.getLeft(), recognition.getTop());
+                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                    recognition.getRight(), recognition.getBottom());
+
+                            // Set the targetZone corresponding to the initial ring stack size
+                            stackSize = recognition.getLabel();
+                            if (stackSize.equals(LABEL_FIRST_ELEMENT)) {
+                                targetZone = WobbleTargetZone.BLUE_C;
+                            } else if (stackSize.equals(LABEL_SECOND_ELEMENT)) {
+                                targetZone = WobbleTargetZone.BLUE_B;
+                            }
+                            telemetry.addData("Target Zone", targetZone);
+
+                        }
                       telemetry.update();
                     }
                 }
